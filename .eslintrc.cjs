8ef8c53402964b7bd123250d484d33a804c5e0f4
SuperArmadilloDev <follietmartin@gmail.com>
2023-07-19 11:06:16 +0200

    more initiation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,41 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:svelte/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        extraFileExtensions: ['.svelte']
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true
+    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            rules:{
+                semi:'error',
+                'prefer-const': 'error',
+                quotes: ['error','single'],
+                'no-console': 'warn'
+            },
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
+            }
+        }
+    ],
+    rules:{
+        semi:'error',
+        quotes: ['error','single'],
+        'no-console': 'warn'
+    },
 };
