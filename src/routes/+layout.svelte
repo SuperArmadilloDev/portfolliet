42d77c8653c865b519f8c089949d3e1b75fe5456
SuperArmadilloDev <follietmartin@gmail.com>
2023-08-03 14:36:42 +0200

    setup for localisation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import { Button } from 'src/components/inputs/index';
+
+  import { onMount } from 'svelte';
   import { page } from '$app/stores';
 
   onMount(() => {
